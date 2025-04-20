f461cac22d3f532a845eb5a11d61db9a7c508d84
Sajidul A. <sajidulahmed664@gmail.com>
2025-04-20 15:21:08 +0530

    Delete src directory

--- src/fluvio.ts
+++
@@ -1,9 +0,0 @@
-import FluvioClient from '@fluvio/client';
-
-const fluvioClient = new FluvioClient({
-  // Replace these with your actual configuration details from Fluvio
-  cluster: 'YOUR_CLUSTER_URL',
-  apiKey: 'YOUR_API_KEY',
-});
-
-export default fluvioClient;
